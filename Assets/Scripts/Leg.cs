c4211cf97348ca472df0531160913b202aadf639
Beau Casemie <0954603@hr.nl>
2019-07-16 17:05:41 +0200

    Prototype Unity Project

---
+++ Assets/Scripts/Leg.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leg : MonoBehaviour
+{
+    private Transform body;
+    private Transform innerLeg, outerLeg;
+    private ConfigurableJoint innerJoint, outerJoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        innerLeg = transform.Find("LegA");
+        innerLeg = transform.Find("LegB");
+
+        innerJoint = innerLeg.GetComponent<ConfigurableJoint>();
+        outerJoint = outerLeg.GetComponent<ConfigurableJoint>();
+
+        body = innerJoint.connectedBody.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
